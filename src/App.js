367c4a8090bf05bb1a452afa978a438cb438fcd4
강석현 <tjrgus0713@gmail.com>
2021-10-13 21:16:44 +0900

    [추가] App.js파일에 만든 페이지 import

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import { GlobalStyle } from "./style/globalStyle";
+import {GlobalStyle} from './style/globalStyle';
+import Main from "./components/main/Main";
+import Portfolio from './components/portfolio/Portfolio';
 function App() {
   return (
-    <GlobalStyle>
-
-    </GlobalStyle>
+    <div>
+      <GlobalStyle/>
+      <Main/>
+      <Portfolio/>
+    </div>
   );
 }
 
