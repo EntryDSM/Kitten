fddd7120ff7de28314670f610726c16b8959d931
kimulchan <leerabi577@gmail.com>
2021-10-16 00:25:23 +0900

    app.js index.js 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import {GlobalStyle} from './style/globalStyle';
 import Main from "./components/main/Main";
 import Portfolio from './components/portfolio/Portfolio';
+import Header from './components/header/index'
+import Footer from './components/footer/index'
+import WhatIsEntry from './components/whatisentry/index'
+import History from './components/history/index'
+import TeamList from './components/teamList';
+import QNA from './components/qna';
 function App() {
   return (
     <div>
       <GlobalStyle/>
+      <Header></Header>
       <Main/>
+      <WhatIsEntry></WhatIsEntry>
       <Portfolio/>
+      <History/>
+      <TeamList></TeamList>
+      <QNA></QNA>
+      <Footer/>
+    
     </div>
   );
 }
